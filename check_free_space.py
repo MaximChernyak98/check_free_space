b097ebbe02c1bc008653b21a988d768daecca040
Maksim Cherniak utk <cherniak_m@utkonos.ru>
2021-09-13 12:54:28 +0300

    dfsds

--- check_free_space.py
+++ check_free_space.py
@@ -1,6 +1,30 @@
 import shutil
+import json
+import requests
+from config import SLACK_WEBHOOK_URL
 
-total, used, free = shutil.disk_usage("/")
-print(f"Total: {total // (2**30)} GiB")
-print(f"Used: {used// (2**30)} GiB")
-print(f"Free: {free// (2**30)} GiB")
+def send_report_to_slack():
+    total, used, free = shutil.disk_usage("/")
+    total_space_in_gb = total// (2**30)
+    used_space_in_gb = used// (2**30)
+    free_space_in_gb = free// (2**30)
+    if free_space_in_gb < 1000:
+        title_of_notification = f'Внимание, заканчивается место на сервере!!!'
+        message_text = f'Total: {total_space_in_gb} GiB\nUsed: {used_space_in_gb} GiB\nFree: {free_space_in_gb} GiB'
+
+        payload = {
+            'attachments': [
+                {
+                    'fallback': title_of_notification,
+                    'pretext': message_text,
+                }
+            ]
+        }
+        headers = {
+            "Content-Type": "application/json",
+        }
+        requests.post(SLACK_WEBHOOK_URL, data=json.dumps(payload, indent=4), headers=headers)
+
+
+if __name__ == '__main__':
+    send_report_to_slack()
